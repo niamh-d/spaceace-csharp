a596c623d648b1f650fcb83c3d0c884d64475f59
Niamh <niamh.linguist@gmail.com>
2025-01-11 12:42:11 +0200

    homing missile scene

---
+++ Scenes/HomingMissile/HomingMissile.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public partial class HomingMissile : HitBox
+{
+	[Export] private float _rotationSpeed = 1.2f;
+	[Export] private float _speed = 40.0f;
+
+	private Player _playerRef;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		_playerRef = GetTree().GetFirstNodeInGroup(Defs.GROUP_PLAYER) as Player;
+		if (_playerRef == null)
+		{
+			QueueFree();
+		}
+	}
+
+	public override void _Process(double delta)
+	{
+		Turn((float)delta);
+	}
+
+	private void Turn(float delta)
+	{
+		var dtp = GlobalPosition.DirectionTo(_playerRef.GlobalPosition);
+		var atp = Transform.X.AngleTo(dtp);
+
+		var amountWeCanRotate = _rotationSpeed * delta;
+		var angleWeWillTurn = Math.Min(Mathf.Abs(atp), amountWeCanRotate);
+
+		Rotate(Mathf.Sign(atp) * angleWeWillTurn);
+		Position += Transform.X * _speed * delta;
+	}
+}
